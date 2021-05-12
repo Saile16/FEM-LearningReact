f9287bc94a5c85e86f64671f4688e3e53fa9c89e
Saile <sailex1030@gmail.com>
2021-05-12 01:26:28 -0500

    Configuration of eslint , and jsx

--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+// const Pet = (props) => {
+//   return React.createElement("div", {}, [
+//     React.createElement("h2", {}, props.name),
+//     React.createElement("h3", {}, props.animal),
+//     React.createElement("h3", {}, props.breed),
+//   ]);
+// };
 
+const Pet = (props) => {
+  const { name, animal, breed } = props;
+  return (
+    <div id="blah">
+      <h2>{name}</h2>
+      <h3>{animal}</h3>
+      <h3>{breed}</h3>
+    </div>
+  );
+};
 export default Pet;
